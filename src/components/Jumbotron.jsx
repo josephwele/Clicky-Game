3422626e86af1dfc77e67d1eb4a2a8995c9153da
josephwele <josiwele@gmail.com>
2019-07-29 15:33:29 -0700

    randomly placing img and css style done

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,7 +3,7 @@ function Jumbotron(prop){
     return(
         <div className="jumbotron jumbotron-fluid">
   <div className="container text-center mt-3 mb-5 pt-3 pb-5">
-    <h1 className="display-5">Clicky Game!</h1>
+    <h1 className="display-5" style={{fontSize:40,fontWeight:"bolder"}}>Clicky Game!</h1>
     <p className="lead ">Click on an image to earn points, but don't click on any more than once!</p>
   </div>
 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,21 @@ class Navbar extends React.Component {
     
 render(){
 const navStyle = {
-  background:'darkslateblue',
-  color:"darkslateblue",
+  background:'darkslateblue'
+}
+const aStyle ={
+  color:'white',
+  fontWeight:'bold',
+  fontSize:30
 }
     return (
         <nav className="navbar  navbar-light py-3" style={navStyle}>
-  <a className="navbar-brand pl-5 font-weight-bolder" href="#">Clicky Game</a>
+  <a className="navbar-brand pl-5 font-weight-bolder" style={aStyle} href="#">Clicky Game</a>
+  <a className="nav-link" style={aStyle} href="#">Click an image to start!</a>      
   <div id="navbarText">
-    <ul className="navbar-nav mr-auto" style={navStyle}>
+    <ul className="navbar-nav">
       <li className="nav-item active text-center">
-        <a className="nav-link float-none" href="#">Click an image to start!</a>
-        <a className="nav-link float-left" href="#"><span>Score:{this.props.scoreParent}<span></span> | Top Score:<span>{this.props.topScoreParent}</span></span></a>
+         <a className="nav-link" style={aStyle}href="#"><span>Score:{this.props.scoreParent}<span></span> | Top Score:<span>{this.props.topScoreParent}</span></span></a>
       </li>
     </ul>
   </div>
